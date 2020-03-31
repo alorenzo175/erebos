d49294e2e368be69c87f13b2b725a22db9fdb7f7
Tony Lorenzo <atlorenzo@email.arizona.edu>
2020-03-31 10:02:16 -0700

    black

--- erebos/cli/train.py
+++ erebos/cli/train.py
@@ -22,15 +22,15 @@ logger = logging.getLogger(__name__)
 def set_env_var(ctx, param, value):
     if not value or ctx.resilient_parsing:
         return
-    envvar = getattr(param, 'set_env_to', param.envvar)
+    envvar = getattr(param, "set_env_to", param.envvar)
     if os.getenv(envvar, "") != value:
         os.environ[envvar] = value
 
 
 class EnvOption(click.Option):
     def __init__(self, *args, **kwargs):
-        if 'set_env_to' in kwargs:
-            self.set_env_to = kwargs.pop('set_env_to')
+        if "set_env_to" in kwargs:
+            self.set_env_to = kwargs.pop("set_env_to")
         super().__init__(*args, **kwargs)
 
 
@@ -84,7 +84,7 @@ def mlflow_options(cmd):
                 expose_value=False,
                 is_eager=True,
                 callback=set_env_var,
-                cls=EnvOption
+                cls=EnvOption,
             ),
             click.option(
                 "--mlflow-s3-access-key-id",
@@ -95,7 +95,7 @@ def mlflow_options(cmd):
                 expose_value=False,
                 is_eager=True,
                 callback=set_env_var,
-                cls=EnvOption
+                cls=EnvOption,
             ),
             click.option(
                 "--mlflow-s3-secret-access-key",
@@ -106,7 +106,7 @@ def mlflow_options(cmd):
                 expose_value=False,
                 is_eager=True,
                 callback=set_env_var,
-                cls=EnvOption
+                cls=EnvOption,
             ),
         ]
         for dec in reversed(decs):
@@ -127,8 +127,8 @@ def train():
 @set_log_level
 @mlflow_options
 def cloud_mask(**kwargs):
-    logger.info('Using tracking URI %s', mlflow.tracking.get_tracking_uri())
+    logger.info("Using tracking URI %s", mlflow.tracking.get_tracking_uri())
     mlflow.set_experiment("erebos-cloud-mask")
     with mlflow.start_run():
-        mlflow.log_param('a', 12)
-        mlflow.log_artifact('rep.json')
+        mlflow.log_param("a", 12)
+        mlflow.log_artifact("rep.json")
