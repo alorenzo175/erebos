baff8a59f85e4d0d3308d873495a3fc7d8b419ea
Antonio Lorenzo <atlorenzo@email.arizona.edu>
2020-04-28 12:30:38 -0700

    add additional optuna tags

--- erebos/ml_models/training/utils.py
+++ erebos/ml_models/training/utils.py
@@ -1,5 +1,7 @@
+import time
 from functools import wraps
 import mlflow
+import optuna
 
 
 def mlflow_callback(study, trial):
@@ -14,14 +16,27 @@ def mlflow_callback(study, trial):
 def log_to_mlflow(f):
     @wraps(f)
     def wrapper(trial):
-        with mlflow.start_run(nested=True):
-            mlflow.log_params(trial.study.user_attrs)
+        with mlflow.start_run(run_name=str(trial.number)):
+            start = time.time()
             score = f(
                 trial,
                 extra_metrics=trial.study.user_attrs.get("extra_metrics", []),
                 save_model=True,
             )
+            duration = time.time() - start
             mlflow.log_params(trial.params)
+            tags = {
+                "trial_number": str(trial.number),
+                "datetime_start": str(trial.datetime_start),
+                "duration": duration,
+            }
+            tags.update(trial.study.user_attrs)
+            tags.update(trial.user_attrs)
+            distributions = {
+                f"{k}_distribution": str(v) for k, v in trial.distributions.items()
+            }
+            tags.update(distributions)
+            mlflow.set_tags(tags)
         return score
 
     return wrapper
